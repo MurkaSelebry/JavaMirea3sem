e69a7ded0acbf7d54cc83716a4c29814152fbd58
selebry <murad1703@yandex.ru>
2022-10-29 11:45:14 +0300

    3rd lab done

---
+++ src/mirea/lab/lab2/BookTest.java
@@ -0,0 +1,10 @@
+package mirea.lab.lab2;
+import java.util.Calendar;
+import java.util.Date;
+public class BookTest {
+        public static void main(String[] args){
+            Book book = new Book("Мертвые души","Гоголь Н.В.", new Date(2018, Calendar.AUGUST,28),978517112);
+            System.out.println(book.toString());
+            book.readBook();
+        }
+}
